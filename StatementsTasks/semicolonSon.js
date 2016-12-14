5a4245799c9b6f1990d8d2e9dc1b4a8a56ed5072
Yauheni Chupilin <e.chupilin@gmail.com>
2016-12-14 03:47:42 +0300

    Add // jshint esversion: 6

--- StatementsTasks/semicolonSon.js
+++ StatementsTasks/semicolonSon.js
@@ -1,4 +1,6 @@
+// jshint esversion: 6
 (function semicolonSon (str) {
+'use strict';
   // SEMICOLON first position must take Array delimiter.
   const SEMICOLON = ':,;';
 
@@ -11,17 +13,17 @@
   while(hasNext(s)) {
     if (isNextObj(s)) {
         s = deleteSemicolonAtStart(s);
-        var key = nextElement(s);
+        let key = nextElement(s);
         s = deleteElementAtStart(s);
         currentArray[key] = [];
         currentArray = currentArray[key];
         s = deleteSemicolonAtStart(s);
     } else {
       s = deleteSemicolonAtStart(s);
-      var key = nextElement(s);
+      let key = nextElement(s);
       s = deleteElementAtStart(s);
       s= deleteSemicolonAtStart(s);
-      var val = nextElement(s);
+      let val = nextElement(s);
       s = deleteElementAtStart(s);
       currentArray[key] = val;
       s = deleteSemicolonAtStart(s);
@@ -31,27 +33,27 @@
   console.log(array);
 
   function hasNext(str) {
-    for(var i in str) {
+    for(let i in str) {
       switch (str[i]) {
-        case SEMICOLON[0]: return true;
-        case SEMICOLON[1]: return true;
-        case SEMICOLON[2]: return true;
+        case SEMICOLON.charAt(0): return true;
+        case SEMICOLON.charAt(1): return true;
+        case SEMICOLON.charAt(2): return true;
       }
     }
     return false;
   }
 
   function nextElement(str) {
-    for(var i in str) {
-      for(var j in SEMICOLON) {
-        if (str[i] == SEMICOLON[j]) return str.slice(0,i);
+    for(let i in str) {
+      for(let j in SEMICOLON) {
+        if (str.charAt(i) == SEMICOLON.charAt(j)) return str.slice(0,i);
       }
     }
     return str;
   }
 
   function deleteSemicolonAtStart(str) {
-    for(var i in SEMICOLON){
+    for(let i in SEMICOLON){
     if (str.charAt(0) == SEMICOLON[i]) {
       return str.slice(1,str.length);
     }
@@ -60,20 +62,20 @@
   }
 
   function deleteElementAtStart(str) {
-    for(var i in str) {
-      for(var j in SEMICOLON) {
-        if (str[i] == SEMICOLON[j]) return str.slice(i,str.length);
+    for(let i in str) {
+      for(let j in SEMICOLON) {
+        if (str.charAt(i) == SEMICOLON.charAt(j)) return str.slice(i,str.length);
       }
     }
     return str;
   }
 
   function isNextObj(str) {
-    for(var i in str) {
+    for(let i in str) {
       switch (str[i]) {
-        case SEMICOLON[0]: return true;
-        case SEMICOLON[1]: return false;
-        case SEMICOLON[2]: return false;
+        case SEMICOLON.charAt(0): return true;
+        case SEMICOLON.charAt(1): return false;
+        case SEMICOLON.charAt(2): return false;
       }
     }
   }
